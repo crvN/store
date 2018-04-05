4e57708f19db6513c1313f49a1f4036b403d1cc7
NIkita Zabluda <nzab@diceus.com>
2018-04-05 11:34:12 +0300

    Updates

--- src/app/model/model.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ProductRepository } from './product.repository';
-import { StaticDataSource } from './static.datasource';
-import { CartService } from '../services/cart.service';
-
-@NgModule({
-  providers: [ProductRepository, StaticDataSource, CartService]
-})
-export class ModelModule { }
